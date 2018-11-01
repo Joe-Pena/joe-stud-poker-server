50757183015a12567f81cda24d6d2c4e3a5b8e8b
Joe-Pena <joepena@protonmail.com>
2018-11-01 13:21:00 -0600

    edit refresh

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const {User} = require('../models/user.model');
 
 //Auth middleware
 const {localPassportMiddleware, jwtPassportMiddleware} = require('../auth/user.strategy');
@@ -24,7 +25,13 @@ authRouter.post('/login', localPassportMiddleware, (req, res) => {
 authRouter.post('/refresh', jwtPassportMiddleware, (req, res) => {
   const user = req.user;
   const jwtToken = createJwtToken(user);
-  res.json({jwtToken, user});
+  let newUser;
+  User.findOne({_id: user.id})
+  .then(resUser => newUser = resUser)
+  .then(() => res.json({jwtToken, newUser}))
+  .catch(err => console.log(err.message));
+
+  // res.json({jwtToken, newUser});
 })
 
 module.exports = authRouter;
